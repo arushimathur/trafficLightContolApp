0ea5ad7f3f577eadedd7ec4378a5c97ac90f8cf6
Arushi Mathur <aarushimathur2015@gmail.com>
2025-12-09 12:41:06 +0530

    Adding comment

--- src/main/java/com/control/trafficlight/TrafficLightControlApplication.java
+++ src/main/java/com/control/trafficlight/TrafficLightControlApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrafficLightControlApplication {
     public static void main(String[] args) {
+        //Creating TrafficLightControlApp
         SpringApplication.run(TrafficLightControlApplication.class, args);
     }
 }
