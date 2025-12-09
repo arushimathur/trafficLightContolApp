1ee9ea970763bde53cb588715fef0c386ed97116
Arushi Mathur <aarushimathur2015@gmail.com>
2025-12-09 11:18:53 +0530

    Adding pom.xml and creating packages

---
+++ src/main/java/com/example/trafficlight/TrafficLightControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.trafficlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficLightControlApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(TrafficLightControlApplication.class, args);
+
+    }
+}
